487b1a910f596adc69f115ae03b2954bc9263238
RunTheLi <lanmarock@gmail.com>
2024-12-09 18:58:01 +0700

    feat: add files js and install date-fns

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+import "./styles.css";
+import { greeting } from "./export.js";
+import { format, formatDistance, formatRelative, subDays } from 'date-fns';
+//import odinImage from "./odin.png";
+   
+//const image = document.createElement("img");
+//image.src = odinImage;
+   
+//document.body.appendChild(image);
+
+console.log(greeting);
