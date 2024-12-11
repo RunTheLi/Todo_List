812dcc70989bc64242017d95d8f8184ffff48942
RunTheLi <lanmarock@gmail.com>
2024-12-11 15:22:30 +0700

    feat: add text

--- src/addproject.js
+++ src/addproject.js
@@ -1,11 +1,10 @@
 //addproject.js
-
 export default function LatestProjectList() {
     const addProjectList = document.getElementById("add-project")
     
     addProjectList.addEventListener('click', () => {
-
+        
     })
 }
 
-// store data
+// more feature 
